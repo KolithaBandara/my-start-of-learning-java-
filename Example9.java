523aa7f8e5625d2eedf97bfaf03b7b95f6147770
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-21 01:53:47 +0530

    Illegal main method declaration
    Step 1 : Create a class.
    Step 2 : Then create the main method.
    Step 3 : This is an illegal Main Method.
    Step 4 : Print "Hello World !" using S.o.p.

---
+++ Example9.java
@@ -0,0 +1,7 @@
+class Example9{
+/*  This is an illegeal Main Method.
+    Because, can't write void between public & static words.*/
+  public void static  main(String []args){
+		System.out.println("Hello World!");
+	}
+}
