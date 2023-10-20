d10078faf5c2bc0f435e848f99873d7ac76f2641
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-21 01:57:09 +0530

    Line Comments

---
+++ Example10.java
@@ -0,0 +1,8 @@
+class Example10{
+	public static void main(String args[]){
+		//This is a Comment
+		System.out.println("Hello World!"); //This is a comment too
+		
+		//These a line comments
+	}
+}
