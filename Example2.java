853e72177afa57760f78cac5be4d7ac68708a5a4
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-21 00:55:51 +0530

    Create Example2.java
    Step 1 : Create a class named "Example2".
    Step 2 : Then create the main method.
    Step 3 : But, this time give some kind of other word for args.
    Step 4 : Print "Hello World !" using S.o.p.

---
+++ Example2.java
@@ -0,0 +1,8 @@
+class Example2{
+  //public static void main(String args[])
+            //can use any kind of name for
+                            //   args
+	public static void main(String John[]){
+		System.out.println("Demon");
+	}
+}
