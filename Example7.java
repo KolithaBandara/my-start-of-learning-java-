db5048af675690516d89f7a00fc1d000e22a24b0
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-21 01:37:23 +0530

    Compiling but Runtime Error in Main Method
    Step 1 : Create a class.
    Step 2 : Then create the main method.
    Step 3 : This time code compiles well without string & args words & without the square brackets [] words. But, It gets a Runtime Error in the Main Method declaration.
    Step 4 : Print "Hello World !" using S.o.p.

---
+++ Example7.java
@@ -0,0 +1,8 @@
+class Example7{
+  /*
+  This time code compiles well without string & args words & without the square brackets [] words.
+  But, It gets a Runtime Error in the Main Method declaration. */
+	public static void main(){
+		System.out.println("Hello World!");
+	}
+}
