5fded357e1de02c22d8ea4f4c85f677a84edd57a
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-21 01:30:09 +0530

    Compiling but Runtime Error in Main Method
    Step 1 : Create a class named "Example2".
    Step 2 : Then create the main method.
    Step 3 : This time code compiles well without public & static words.
    But, It gets a Runtime Error in the Main Method declaration.
    Step 4 : Print "Hello World !" using S.o.p.

---
+++ Example6.java
@@ -0,0 +1,7 @@
+class Example6{
+  /* This time code compiles well without public & static words.
+  But, It gets a Runtime Error in the Main Method declaration. */
+	void main(String args[]){
+		System.out.println("Hello World!");
+	}
+}
