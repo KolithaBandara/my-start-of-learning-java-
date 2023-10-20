9a62c61ff449958ae65f53cda4847d324c60a0cb
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-21 01:06:20 +0530

    Create Example3.java
    Step 1 : Create a class named "Example3".
    Step 2 : Then create the main method.
    Step 3 : But, this time you can put these square brackets[] middle of String and args words.
    Step 4 : Print "Hello World !" using S.o.p.

---
+++ Example3.java
@@ -0,0 +1,8 @@
+class Example{
+//public static void main(String args[])
+  //You can put these square brackets[] middle of 
+    // ----------------> String and args words.
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+}
