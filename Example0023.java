01480d476bc4eecdb4b877440427ea8ce040600b
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 00:18:08 +0530

    Integer, Character, String and Floating-point Literals

---
+++ Example0023.java
@@ -0,0 +1,8 @@
+class Example0023{
+  public static  void main(String []args){
+		System.out.println(1); //Integer
+		System.out.println('1'); //Character
+		System.out.println("1"); //String
+		System.out.println(1.0); //Floating-point
+	}
+}
