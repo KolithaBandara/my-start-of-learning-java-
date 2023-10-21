a906d7cb394e69a40c5d1e0d0265749458d7b6b2
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 03:18:10 +0530

    declaring and initializing same data type variables in single line

---
+++ Example0048.java
@@ -0,0 +1,10 @@
+class Example0048{
+	public static  void main(String []args){
+		int x=100,y,z=300; //declaring and initializing same data type variables in a single line.
+		y=200; //initializing 
+    
+		System.out.println(x);//100
+		System.out.println(y);//200
+		System.out.println(z);//200
+	} 
+}
