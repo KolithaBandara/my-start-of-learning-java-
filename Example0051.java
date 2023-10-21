9d2ab81a86b9d1fd9433b20af82eb428e2bb68ef
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 03:27:58 +0530

    variable assign to variable

---
+++ Example0051.java
@@ -0,0 +1,14 @@
+class Example{
+	public static  void main(String []args){
+		int x=100; //declaring and initializing
+		int y=200; //declaring and initializing
+		System.out.println(x); //100
+		System.out.println(y); //200
+
+		x=y; //assign value of y to location x
+    
+		System.out.println(x); //200
+		System.out.println(y); //200
+
+	} 
+}
