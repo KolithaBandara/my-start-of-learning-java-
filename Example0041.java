9d632af814b25f777f6177848ed464a7d015cddf
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 02:46:44 +0530

    declaring, initializing, character & String literals

---
+++ Example0041.java
@@ -0,0 +1,8 @@
+class Example0041{
+	public static  void main(String []args){
+		int x; //declaring
+		x=100; //initializing
+		System.out.println('x'); //Character Literal
+		System.out.println("x"); //String Literal
+	} 
+}
