332b103f91f3ead36a8787035625276da4741fcf
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 03:20:29 +0530

    Variable declaring & initializing

---
+++ Example0049.java
@@ -0,0 +1,9 @@
+class Example{
+	public static  void main(String []args){
+		int x,y,z; //declaring same data type variables in a single line
+		x=y=z=100; //inizializing. Assignning same data to variables in a single line
+		System.out.println(x);//100
+		System.out.println(y);//100
+		System.out.println(z);//100
+	} 
+}
