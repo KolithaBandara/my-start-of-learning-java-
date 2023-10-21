1115ee45222ddddb965c0e12e7c0fc43d78280c1
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 03:04:46 +0530

    Variable compile Error

---
+++ Example0043.java
@@ -0,0 +1,8 @@
+class Example{
+	public static  void main(String []args){
+		int x; //declaring
+		System.out.println(x); //Illegal
+		x=100; //initializing
+		System.out.println(x); //Legal
+	} 
+}
