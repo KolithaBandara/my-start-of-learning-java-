ae1350ebeafe4427de3e0a17ab50edd69d5d553c
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 04:29:09 +0530

    String Concatenation vs Arithmetic Addition

---
+++ Example0054.java
@@ -0,0 +1,6 @@
+class Example0054{
+	public static  void main(String []args){
+		System.out.println(10+20); //prints 30 (Arithmetic)
+		System.out.println("AB"+"CD"); //prints ABCD (String Concat)
+	} 
+}
