4725c33aec4f8a11b68890824195cd85401d1e74
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-21 13:45:44 +0530

    Java String Literals

---
+++ Example0018.java
@@ -0,0 +1,7 @@
+class Example0018{
+  public static void main(String args[]){
+    System.out.println("John");  //String Literals
+    System.out.println("A");    //String Literals
+    System.out.println("78698");  //String Literals
+  }
+}
