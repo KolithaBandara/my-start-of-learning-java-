b4a625d93d4d841318891fdc4591c72385e6b629
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 00:12:48 +0530

    Java Character Literal Compile Error

---
+++ Example0021.java
@@ -0,0 +1,9 @@
+class Example0021{
+	public static void main(String args[]){
+    //Compile Error
+		System.out.println('AB');
+    /* You can't write more than one character 
+                        in
+          Java Caharacter Literal type*/
+	}
+}
