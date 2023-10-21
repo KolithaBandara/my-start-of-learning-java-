51d71819241f952285614d0c43233bc72db6de06
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 00:23:33 +0530

    Escape Characters in Java

---
+++ Example0024.java
@@ -0,0 +1,8 @@
+class Example0024{
+  public static  void main(String []args){
+		System.out.println("ABCD"); //String Literal
+		System.out.println("AB\bCD"); //String Literal ---> But using "\b" (Backspace Character) to backspace B
+		System.out.println("AB\tCD"); //String Literal ---> But, using "\t" to insert a new tab 
+		System.out.println("AB\nCD"); //String Literal ---> But, using "\n" to insert a new line
+	}
+}
