b8a22ef0c5e126e0d130b79a5afcdbcf2878d205
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 04:30:46 +0530

    string concat and arithmetic addition

---
+++ Example0055.java
@@ -0,0 +1,8 @@
+class Example0055{
+	public static  void main(String []args){
+		System.out.println(10+20); //prints 30 (Arithmetic)
+		System.out.println("10"+"20"); //prints 1020 --> String concat
+		System.out.println(10+"20"); //prints 1020  --> String concat
+		System.out.println("10"+20); //prints 1020  --> String concat
+	} 
+}
