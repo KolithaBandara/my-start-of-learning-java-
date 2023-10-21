7a54340440476ae83ad7afa3e0c71b8f39b0c1d0
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 02:40:38 +0530

    Variable declaring, initializing, displaying

---
+++ Example0040.java
@@ -0,0 +1,7 @@
+class Example0040{
+	public static void main(String []args){
+		int x; //Declaring the variable x.
+		x=100; //Initializing the variable x.
+		System.out.println(x); //Displaying the variable x.
+	} 
+}
