5ae5cf54cac84ec7a5a1651834e229bac4ed9736
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-21 13:43:21 +0530

    Java Character literal

---
+++ Example0017.java
@@ -0,0 +1,7 @@
+class Example0017{
+  public static void main(String args[]){
+    System.out.println('A');  //Character literal
+    System.out.println('a');  //Character literal
+    System.out.println('1');  //Character literal
+  }
+}
