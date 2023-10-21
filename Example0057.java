b1c134f9a8913f401ccd8577a1c010721f5b3ee1
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 04:37:55 +0530

    string concatenation and arithmetic addition

---
+++ Example0057.java
@@ -0,0 +1,11 @@
+class Example0057{
+	public static  void main(String []args){
+		int x=10,y=20; //declaring same data type variables in single line
+		System.out.println("x+y"); //x+y
+		System.out.println("x"+"y"); //xy
+		System.out.println(x+"y"); //10y
+		System.out.println("x"+y); //x20
+		System.out.println(x+y); //30
+
+	} 
+}
