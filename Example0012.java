e5f274cc043ffa78c78c7a2d22c29dd6d90b078e
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-21 13:16:56 +0530

    System.out.print

---
+++ Example0012.java
@@ -0,0 +1,13 @@
+class Example0012{
+  public static void main(String args[]){
+    System.out.print("A");
+    System.out.print("B");
+    System.out.print("C");
+    System.out.print("D");
+    System.out.print("E");
+    System.out.print("F");
+    System.out.print("G");
+    System.out.print("H");
+    System.out.print("I");
+  }
+}
