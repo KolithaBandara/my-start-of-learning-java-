5c14f30f356c74ac5e05ac7b1917ef931f7617f0
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 03:09:18 +0530

    Variable Overwriting

---
+++ Example0045.java
@@ -0,0 +1,9 @@
+class Example{
+	public static  void main(String []args){
+		int x; //declaring
+		x=100; //initializing
+		System.out.println(x);//100
+		x=200; // overwriting
+		System.out.println(x);//200
+	} 
+}
