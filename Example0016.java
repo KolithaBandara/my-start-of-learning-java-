0bd21d740923229efcfdec2ede52b1bb2897659f
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-21 13:31:35 +0530

    Java Floating-point Literals

---
+++ Example0016.java
@@ -0,0 +1,7 @@
+class Example0016{
+  public static void main(String args[]){
+    System.out.println(1.1234);  //Floating-point Literal
+    System.out.println(-1.1234); //Floating-point Literal
+    System.out.println(0.00123);  //Floating-point Literals
+  }
+}
