2e3263555a71f0a32ea2dd6c20d1221020af0064
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 03:15:50 +0530

    Declaring same type variables in a single Line

---
+++ Example0047.java
@@ -0,0 +1,13 @@
+class Example0047{
+	public static  void main(String []args){
+		int x,y,z; // declaring same tye variables in a single line.
+		
+		x=100; //initializing
+		y=200; //initializing
+		z=300; //initializing
+    
+		System.out.println(x);//100
+		System.out.println(y);//200
+		System.out.println(z);//200
+	} 
+}
