ab0ab2e33868b7d66cfca6dbcebfed9e5c97482f
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-21 13:19:36 +0530

    System.out.println() VS System.out.print()

---
+++ Example0013.java
@@ -0,0 +1,13 @@
+class Example0013{
+  public static void main(String args[]){
+    System.out.println("A");
+    System.out.print("B");
+    System.out.print("C");
+    System.out.println("D");
+    System.out.println("E");
+    System.out.print("F");
+    System.out.print("G");
+    System.out.println("H");
+    System.out.println("I");
+  }
+}
