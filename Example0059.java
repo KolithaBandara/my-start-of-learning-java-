6241d9af091dc88643da043755854bf712a42180
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 04:45:41 +0530

    exercise

---
+++ Example0059.java
@@ -0,0 +1,6 @@
+class Example0059{
+	public static  void main(String []args){
+		int age=23; //declaring and initializing variable name as "age".
+		System.out.println("Your age is : "+age); //Your age is : 23
+	} 
+}
