e24db0dafe9f60599d1be2f5800081683785c7de
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 00:39:30 +0530

    Unclosed & Empty Character Literal

---
+++ Example0030.java
@@ -0,0 +1,7 @@
+class Example0030{
+  public static  void main(String []args){
+		System.out.println('''); //Illegal
+		/* 1. Empty Character Literal
+        2. Unclosed Character Literal */
+	} 
+}
