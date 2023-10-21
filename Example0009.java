b50084c00b748ccecaa2999df45f2c55a585b185
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-21 14:15:17 +0530

    Dealing with Java Main Method

--- Example0009.java
+++ Example0009.java
@@ -1,6 +1,6 @@
 class Example9{
-/*  This is an illegeal Main Method.
-    Because, can't write void between public & static words.*/
+/*  This is an illegal Main Method.
+    Because can't write void between public & static words.*/
   public void static  main(String []args){
 		System.out.println("Hello World!");
 	}
