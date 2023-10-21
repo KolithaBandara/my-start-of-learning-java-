74d822af8686e7dd3c09f338051a41ee82b90caa
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 03:12:54 +0530

    Variable declaring, initializing

---
+++ Example0046.java
@@ -0,0 +1,11 @@
+class Example0046{
+	public static  void main(String []args){
+		int x; //declaring
+		x=100; //initializing
+		System.out.println(x);//100
+		
+		int y; //declaring
+		y=200; //initializing
+		System.out.println(y);//200
+	} 
+}
