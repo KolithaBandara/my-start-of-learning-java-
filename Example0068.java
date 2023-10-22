c302d525329cd8ad8a689da35d7f60ef76dbf32b
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-22 14:45:07 +0530

    String is non-primitive data type

---
+++ Example0068.java
@@ -0,0 +1,14 @@
+class Example0068{
+	public static  void main(String []args){
+		String name="Dean Winchester";
+		System.out.println(name); //
+	}		 
+}
+//String is not a primitive data type, is an Object, Class
+
+/*
+The string data type is a non-primitive data type but it is predefined in java,
+some people also call it a special ninth primitive data type.
+This solves the case where a char cannot store multiple characters, 
+a string data type is used to store the sequence of characters.
+*/
