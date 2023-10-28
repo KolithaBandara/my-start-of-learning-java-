ae7548167d45e87ff238d56af9d21729bff85533
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-28 23:51:18 +0530

    Example for Arithmetic Operators

---
+++ Example0082.java
@@ -0,0 +1,17 @@
+import java.util.*; 
+class Example{
+	public static  void main(String []args){
+		int x=100,y=50,z;
+		z=x+y;
+		System.out.println(x+" + "+y+" = "+z); //100 + 50 = 150
+
+		z=x-y;
+		System.out.println(x+" - "+y+" = "+z); //100 - 50 = 50
+
+		z=x*y;
+		System.out.println(x+" * "+y+" = "+z); //100 * 50 = 5000
+
+		z=x/y;
+		System.out.println(x+" / "+y+" = "+z); //100 / 50 = 2
+	}
+}
