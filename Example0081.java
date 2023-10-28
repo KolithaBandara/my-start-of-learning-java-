e2816ad1181c27f1cbcb4144ca83a7857291049c
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-28 23:48:13 +0530

    Example for arithmetic

---
+++ Example0081.java
@@ -0,0 +1,15 @@
+import java.util.*; 
+class Example0081{
+	public static  void main(String []args){
+		int x=100;
+		System.out.println(x); //100
+		System.out.println(+x); //100
+		System.out.println(-x); //-100
+		System.out.println(x); //100
+
+		x=-100;
+		System.out.println(x);//-100
+		System.out.println(-x);//100
+
+	}
+}
