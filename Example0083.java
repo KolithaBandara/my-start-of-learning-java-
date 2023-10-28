c796a0e4381a4ac77d6f9d8c630c5991635348cf
Kolitha Bandara <138357025+KolithaBandara@users.noreply.github.com>
2023-10-28 23:54:11 +0530

    Example for Arithmetic Operators

---
+++ Example0083.java
@@ -0,0 +1,19 @@
+import java.util.*; 
+class Example0083{
+	public static  void main(String []args){
+		int x=10 % 2 ;
+		System.out.println(x); //0
+
+		x=10 % 3 ;
+		System.out.println(x); //1
+
+		x=10 % 4 ;
+		System.out.println(x); //2
+
+		x=10 % 6 ;
+		System.out.println(x); //4
+
+		x=10 % 12 ;
+		System.out.println(x); //10
+	}
+}
